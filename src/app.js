47b73d86fee6d0bd5a961e3760d10877d3fe08e8
Stanley2000 <stanleykelechi2000@gmail.com>
2024-03-20 12:48:49 +0100

    add() Notfound middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const rootRoutes = require('./routes/index.route');
-const authRoutes = require('./routes/auth.route')
-const errHandler = require('./middlewares/error.middleware')
-const _ = require('./config')
-const cors = require('cors')
-const compression = require('compression')
+const bodyParser = require("body-parser");
+const rootRoutes = require("./routes/index.route");
+const authRoutes = require("./routes/auth.route");
+const errHandler = require("./middlewares/error.middleware");
+const { NotFound } = require("./helpers/exceptions/error.helper");
+const _ = require("./config");
+const cors = require("cors");
+const compression = require("compression");
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/api/v1',rootRoutes)
-app.use('/api/v1/auth',authRoutes)
-app.use(errHandler)
-app.use(cors({optionsSuccessStatus:200,origin:_.client}))
-app.use(compression())
+app.use("/api/v1", rootRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.all("*", (req, res, next) => {
+  next(
+    new NotFound(`Can't find the URL: ${req.originalUrl} in my server`, {
+      path: "URL",
+      value: req.originalUrl,
+      field: "URL path",
+    })
+  );
+});
+app.use(errHandler);
+app.use(cors({ optionsSuccessStatus: 200, origin: _.client }));
+app.use(compression());
 
-module.exports = app
+module.exports = app;
